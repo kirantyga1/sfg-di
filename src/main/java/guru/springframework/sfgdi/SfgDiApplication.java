5414370fa0243891594298b60f43ab84c4323cb7
kiran <kirantyga@gmail.com>
2021-01-31 23:28:07 -0500

    Spring Profile examples

--- src/main/java/guru/springframework/sfgdi/SfgDiApplication.java
+++ src/main/java/guru/springframework/sfgdi/SfgDiApplication.java
@@ -1,9 +1,6 @@
 package guru.springframework.sfgdi;
 
-import guru.springframework.sfgdi.controller.MyController;
-import guru.springframework.sfgdi.controller.PropertyInjectedController;
-import guru.springframework.sfgdi.controller.ConstructionInjectionController;
-import guru.springframework.sfgdi.controller.SetterBasedController;
+import guru.springframework.sfgdi.controller.*;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
@@ -11,26 +8,27 @@ import org.springframework.context.ApplicationContext;
 @SpringBootApplication
 public class SfgDiApplication {
 
-    public static  void main(String[] args) {
+    public static void main(String[] args) {
         ApplicationContext ctx = SpringApplication.run(SfgDiApplication.class, args);
 
-        MyController myController = (MyController) ctx.getBean("myController");
+        I18nController i18nController = (I18nController) ctx.getBean("i18nController");
+        System.out.println(i18nController.sayHello());
 
+        MyController myController = (MyController) ctx.getBean("myController");
         System.out.println("--primaryGreetingService");
         System.out.println(myController.sayHello());
 
 
-
         System.out.println("Property---------------");
-        PropertyInjectedController  propertyInjectedController= (PropertyInjectedController) ctx.getBean("propertyInjectedController");
+        PropertyInjectedController propertyInjectedController = (PropertyInjectedController) ctx.getBean("propertyInjectedController");
         System.out.println(propertyInjectedController.getGreeting());
 
         System.out.println("Setter Injection---------------");
-        SetterBasedController setterBasedController= (SetterBasedController) ctx.getBean("setterBasedController");
+        SetterBasedController setterBasedController = (SetterBasedController) ctx.getBean("setterBasedController");
         System.out.println(setterBasedController.getGreeting());
 
         System.out.println("Constructor Injection---------------");
-        ConstructionInjectionController constructionInjectionController= (ConstructionInjectionController) ctx.getBean("constructionInjectionController");
+        ConstructionInjectionController constructionInjectionController = (ConstructionInjectionController) ctx.getBean("constructionInjectionController");
         System.out.println(constructionInjectionController.getGreeting());
 
 
